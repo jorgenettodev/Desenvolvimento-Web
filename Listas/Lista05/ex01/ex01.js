87b8b0b9a9da3d3543e76f9486d662deacce4ec4
Jorgenettodev <jorgenetto96@gmail.com>
2024-05-21 05:35:03 -0300

    Finalizando a lista 05

---
+++ Listas/Lista05/ex01/ex01.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome : ");
+        let idade = Number(prompt("Digite sua idade : "));
+        let empregoAtual = prompt("Digite seu emprego atual : ");
+        let endereco = prompt("Digite seu endereco : ");
+
+        alert(`Seu nome é ${nome} \nVocê tem ${idade} anos \nVocê trabalha como ${empregoAtual} \ne possui domicílio no Endereço ${endereco}`);
---
+++ Listas/Lista05/ex02/ex02.js
@@ -0,0 +1,6 @@
+let nome = "Joe Rogan";
+let idade = 47;
+let empregoAtual = "podcaster";
+let endereco = "Los Angeles";
+
+console.log(`Seu nome eh: ${nome} \nVoce tem ${idade} anos \nTrabalha como ${empregoAtual} \nMora no Endereco ${endereco}`);
